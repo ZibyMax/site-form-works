8537ebc0c1a5d3e51c0330c0408001b97d73066d
ZibyMax <max@rekstar-nn.ru>
2019-04-24 18:13:38 +0300

    передал форму в шаблон

--- credit_calc/app/views.py
+++ credit_calc/app/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from .forms import CalcForm
@@ -5,4 +6,9 @@ from .forms import CalcForm
 
 class CalcView(TemplateView):
     template_name = "app/calc.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CalcForm()
+        return context
 
